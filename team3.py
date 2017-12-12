b765ced9c61b9977ba5170d6ebcb204bfda5d1f6
TRedwine <34242146+TRedwine@users.noreply.github.com>
2017-12-12 08:55:51 -0600

    Team 3
    First betray, then if they collude betray, and if we both betray betray then we collude.

--- team3.py
+++ team3.py
@@ -6,9 +6,10 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'loneRanger' # Only 10 chars displayed.
+strategy_name = 'Opposite'
+strategy_description = '''if they collude then the program will betray and if 
+they betray then the program will randomly choose between betray or collude.'''
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -26,7 +27,16 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
+    if len(my_history) == 0: #It's the first round of the game.
+        return 'b'
+    elif their_history == 'c':
+        return 'b' #betray if they collude 
+    elif my_history == 'b' and their_history == 'b':
+        return 'c' #If both betrayed then collude
+    else:
+        return 'b' #otherwise betray
+    
+        
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
@@ -54,7 +64,7 @@ if __name__ == '__main__':
               their_score=0,
               result='b'):
          print 'Test passed'
-     # Test 2: Continue betraying if they collude despite being betrayed.
+     # Test 2: Randomly choose betweeen collude and betray when they collude.
     test_move(my_history='bbb',
               their_history='ccc', 
               # Note the scores are for testing move().
